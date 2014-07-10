c26fa620fb2928736d3a862acb81902d5548ef20
digiholic <digikun@gmail.com>
2014-07-10 10:48:26 -0600

    Initialization of Rails.
    rails new bcdungeon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bcdungeon::Application.config.secret_token = '3abbc9ef14c7a0a4ef948edaa9753ae1c8c0be8907637888345138ae7c5f9f1abc0a8546fea7008c8961406cc54c943dcc5a7f2fc0c062d9921cd98b96bfdbf5'
